5c55f3d466761bcd382bc9c150e13cfe9640e23b
= <jsznawa@naver.com>
2022-02-09 20:35:49 +0900

    SWEA 1873 상호의 배틀필드(수정)

--- SWEA_1873.java
+++ SWEA_1873.java
@@ -1,11 +1,7 @@
-package com;
-
-import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class SWEA_1873 {
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("./src/input_1873.txt"));
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
