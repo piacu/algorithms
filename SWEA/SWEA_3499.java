6525a4fc45aec6153f7b3d29bbe84bdc869dc133
= <jsznawa@naver.com>
2022-02-10 01:53:44 +0900

    SWEA 3499. 퍼펙트 셔플

--- SWEA/SWEA_3499.java
+++ SWEA/SWEA_3499.java
@@ -17,16 +17,16 @@ public class SWEA_3499 {
 			int nums = Integer.parseInt(bf.readLine());
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			String[] words = new String[nums];
-			int oe = nums%2==0 ? 0 : 1;
+			int oe = nums%2==0 ? 0 : 1; // 짝수이면 0, 홀수이면 1
 			
 			for(int i=0; i<nums; i++) {
 				words[i] = st.nextToken();
 			}
 			
 			System.out.print("#"+tc+" ");
-			for(int i=0; i<nums/2+oe; i++) {
+			for(int i=0; i<nums/2+oe; i++) { // 홀수면 nums/2+1번
 				System.out.print(words[i]+" ");
-				if(i<nums/2)
+				if(i<nums/2) // 홀수면 nums/2 보다 1번 작게 동작
 					System.out.print(words[nums/2+i+oe]+" ");
 			}
 			
